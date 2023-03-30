5c24b73111a9abf5fc35274fccaa892b713d6be1
haram8009 <haram8009@gmail.com>
2023-03-30 19:24:06 +0900

    chore

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/<int:blog_id>',views.detail,name='detail'),
-    path('blog/new',views.new,name='new'),
-    path('blog/create',views.create,name='create'),
+    path('blog/<int:blog_id>', views.detail, name='detail'),
+    path('blog/new', views.new, name='new'),
+    path('blog/create', views.create, name='create'),
+
 ]
--- session/blog/views.py
+++ session/blog/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render,get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
+
 
 def home(request):
     blogs = Blog.objects.all()
-    return render(request,'home.html',{'blogs':blogs})
+    return render(request, 'home.html', {'blogs': blogs})
 
-def detail(request,blog_id):
-    blog = get_object_or_404(Blog,pk=blog_id)
-    return render(request,'detail.html',{'blog':blog})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html', {'blog': blog})
+
 
 def new(request):
-    return render(request,'new.html')
-    
+    return render(request, 'new.html')
+
+
 def create(request):
     new_blog = Blog()
     new_blog.title = request.POST['title']
     new_blog.content = request.POST['content']
     new_blog.save()
-    return redirect('detail',new_blog.id)
+    return redirect('detail', new_blog.id)
