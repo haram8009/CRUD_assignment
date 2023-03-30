5fe37e9424db504a0093289f51526f01e83a39cc
haram8009 <haram8009@gmail.com>
2023-03-30 19:40:56 +0900

    add update

--- session/blog/urls.py
+++ session/blog/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('blog/<int:blog_id>', views.detail, name='detail'),
     path('blog/new', views.new, name='new'),
     path('blog/create', views.create, name='create'),
-
+    path('blog/edit/<int:blog_id>', views.edit, name='edit'),
+    path('blog/update/<int:blog_id>', views.update, name="update"),
 ]
--- session/blog/views.py
+++ session/blog/views.py
@@ -22,3 +22,16 @@ def create(request):
     new_blog.content = request.POST['content']
     new_blog.save()
     return redirect('detail', new_blog.id)
+
+
+def edit(request, blog_id):
+    edit_blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'edit.html', {'edit_blog': edit_blog})
+
+
+def update(request, blog_id):
+    old_blog = get_object_or_404(Blog, pk=blog_id)
+    old_blog.title = request.POST['title']
+    old_blog.content = request.POST['content']
+    old_blog.save()
+    return redirect('detail', old_blog.id)
