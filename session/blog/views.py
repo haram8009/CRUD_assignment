784d2d7ab1ce90266f3b158b607f50970ff588d6
haram8009 <haram8009@gmail.com>
2023-04-03 12:40:46 +0900

    form 추가

--- session/blog/views.py
+++ session/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Blog
+from .forms import BlogForm
 
 
 def home(request):
@@ -16,12 +17,21 @@ def new(request):
     return render(request, 'new.html')
 
 
+# def create(request):
+#     new_blog = Blog()
+#     new_blog.title = request.POST['title']
+#     new_blog.content = request.POST['content']
+#     new_blog.save()
+#     return redirect('detail', new_blog.id)
+
 def create(request):
-    new_blog = Blog()
-    new_blog.title = request.POST['title']
-    new_blog.content = request.POST['content']
-    new_blog.save()
-    return redirect('detail', new_blog.id)
+    form = BlogForm(request.POST)
+    # good case
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.save()
+        return redirect('detail', new_blog.id)
+    return render(request, 'new.html')
 
 
 def edit(request, blog_id):
@@ -29,12 +39,23 @@ def edit(request, blog_id):
     return render(request, 'edit.html', {'edit_blog': edit_blog})
 
 
+# def update(request, blog_id):
+#     old_blog = get_object_or_404(Blog, pk=blog_id)
+#     old_blog.title = request.POST['title']
+#     old_blog.content = request.POST['content']
+#     old_blog.save()
+#     return redirect('detail', old_blog.id)
+
 def update(request, blog_id):
     old_blog = get_object_or_404(Blog, pk=blog_id)
-    old_blog.title = request.POST['title']
-    old_blog.content = request.POST['content']
-    old_blog.save()
-    return redirect('detail', old_blog.id)
+    form = BlogForm(request.POST, instance=old_blog)
+    # good case
+    if form.is_valid():
+        new_blog = form.save(commit=False)
+        new_blog.save()
+        return redirect('detail', old_blog.id)
+    # bad case
+    return render(request, "new.html")
 
 
 def delete(request, blog_id):
